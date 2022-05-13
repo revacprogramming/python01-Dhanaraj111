6f9a02b5c2a4b5744a210018a92d5bd9deb55123
Dhanaraj__ 011 <95837210+Dhanaraj111@users.noreply.github.com>
2022-05-13 09:28:59 +0000

    activitySet 02

--- ActivitySet02/problem03.py
+++ ActivitySet02/problem03.py
@@ -1,19 +1,11 @@
+hrs=input('enter hrs')
+h=float(hrs)
+rate=input('enter rate')
+r=float(rate)
+if h<=40:
+    pay=h*r
+else:
+    pay=40*r
+    pay=pay+((h-40)*(1.5*r))
 
-
-def get_cs():
-    """get string input"""
-
-
-def cs_to_lot(cs):
-    """convert connected string to list of strings"""
-
-
-def main():
-    cs = get_cs()
-
-    lot = cs_to_lot(cs)
-    print(lot)
-
-
-if __name__ == '__main__':
-    main()
+print(pay)
